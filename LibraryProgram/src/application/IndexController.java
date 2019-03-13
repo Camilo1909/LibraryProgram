21918a3bd335cc7a42caab70c32fee79823ceca4
Jhonnier20 <45016617+Jhonnier20@users.noreply.github.com>
2019-03-13 16:34:28 -0500

    Controller

--- LibraryProgram/src/application/IndexController.java
+++ LibraryProgram/src/application/IndexController.java
@@ -1,5 +1,18 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
 public class IndexController {
+	
+	@FXML
+    void newClient(ActionEvent event) {
+
+    }
+
+    @FXML
+    void pay(ActionEvent event) {
+
+    }
 	
 }
