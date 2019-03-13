7f6e2709b1f139c794af9ea35d57af069c9e8a08
1193415440 <1193415440@REDES07.academico.icesi.edu.co>
2019-03-13 17:05:55 -0500

    Fx

--- LibraryProgram/src/application/IndexController.java
+++ LibraryProgram/src/application/IndexController.java
@@ -1,13 +1,41 @@
 package application;
 
+import java.util.Optional;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextInputDialog;
 
 public class IndexController {
 	
+	
+	
+	
+	public void initialize() {
+		
+	}
+	
+	@FXML
+	void selectBooks() {
+		
+	}
+	
+	
 	@FXML
     void newClient(ActionEvent event) {
+		TextInputDialog dialog = new TextInputDialog("walter");
+		dialog.setTitle("Text Input Dialog");
+		dialog.setHeaderText("Look, a Text Input Dialog");
+		dialog.setContentText("Please enter your name:");
 
+		// Traditional way to get the response value.
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()){
+		    System.out.println("Your name: " + result.get());
+		}
+
+		// The Java 8 way to get the response value (with lambda expression).
+		result.ifPresent(name -> System.out.println("Your name: " + name));
     }
 
     @FXML
